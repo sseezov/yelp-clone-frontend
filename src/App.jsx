3da1cf7378fe865a35e2f6c7608a10a53c346270
sseezov <sseezov@mail.ru>
2022-10-07 22:36:59 +0700

    feat: context added, styles added

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import UpdatePage from './routes/UpdatePage'
 import Home from './routes/Home'
 import RestaurantDetailPage from './routes/RestaurantDetailPage'
 import '../src/styles.css'
+import { RestaurantsContextProvider } from './context/restaurantsContext'
 
 const App = () => {
   return (
-    <BrowserRouter >
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
-        <Route path="/restaurants/:id/update" element={<UpdatePage />} />
-      </Routes>
-    </BrowserRouter >
+    <RestaurantsContextProvider>
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+          <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+        </Routes>
+      </BrowserRouter >
+    </RestaurantsContextProvider>
+
 
   )
 }
--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -2,71 +2,93 @@ import React from 'react'
 
 const RestaurantList = () => {
   return (
-    <div>
+    <div className='restaurant-list'>
       <table>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-
-        </tr>
+        <thead>
+          <tr>
+            <th>Ресторан</th>
+            <th>Место</th>
+            <th>Цена</th>
+            <th>Рейтинг</th>
+            <th>Редактировать</th>
+            <th>Удалить</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>McDonalds</td>
+            <td>New York</td>
+            <td>$$</td>
+            <td>rating</td>
+            <td>
+              <button>update</button>
+            </td>
+            <td>
+              <button>delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>McDonalds</td>
+            <td>New York</td>
+            <td>$$</td>
+            <td>rating</td>
+            <td>
+              <button>update</button>
+            </td>
+            <td>
+              <button>delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>McDonalds</td>
+            <td>New York</td>
+            <td>$$</td>
+            <td>rating</td>
+            <td>
+              <button>update</button>
+            </td>
+            <td>
+              <button>delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>McDonalds</td>
+            <td>New York</td>
+            <td>$$</td>
+            <td>rating</td>
+            <td>
+              <button>update</button>
+            </td>
+            <td>
+              <button>delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>McDonalds</td>
+            <td>New York</td>
+            <td>$$</td>
+            <td>rating</td>
+            <td>
+              <button>update</button>
+            </td>
+            <td>
+              <button>delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>McDonalds</td>
+            <td>New York</td>
+            <td>$$</td>
+            <td>rating</td>
+            <td>
+              <button>update</button>
+            </td>
+            <td>
+              <button>delete</button>
+            </td>
+          </tr>
+          
+        </tbody>
 
       </table>
     </div>
